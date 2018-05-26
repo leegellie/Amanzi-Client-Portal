a66ac3f7b8be87ed0c75e58157aefb2223bdc650
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-05-26 12:44:58 -0400

    DB Connection

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?
 //DATABASE CONNECTION INFO
 define('db_host','172.30.200.249');
-define('db_user','user001');
-define('db_password','cL4MpUjCtNdNRoYY');
+define('db_user','user553');
+define('db_password','Tjl3Pf72Ct6HUqvX');
 define('db_name','Portal');
 	
 //ERROR REPORTING
