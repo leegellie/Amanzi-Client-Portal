606d33662480eab38a2d061a835c2f3adc4d8a43
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-05-31 13:32:06 -0400

    e

--- include/class/user.class.php
+++ include/class/user.class.php
@@ -573,7 +573,7 @@ class user_action {
 		$sql = 'SELECT id, username, fname, lname, email FROM users WHERE company LIKE "Amanzi"';
 		if ( $a['user'] == 'manager' ) {
 			if ( $a['department'] == 0 ) {
-				$sql .= ' AND division LIKE "' . $a['division'] . '"';
+				$sql .= ' AND division LIKE ' . $a['division'];
 			} else {
 				$sql .= ' AND department LIKE "' . $a['department'] . '"';
 			}
