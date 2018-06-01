babadfb9f13696000ddd638e55bd121ec3960bb0
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-05-31 20:18:22 -0400

    1

--- admin/projects.php
+++ admin/projects.php
@@ -2196,12 +2196,12 @@ $(document).ready(function(){
               //console.log(value['detail']);
               $.each(value['detail'], function(k,v){
                 //console.log(v['job_sqft']);
-				  console.log(v['job_sqft']);
+				  console.log('v=' + v['job_sqft']);
                 sum_sqft += parseInt(v['job_sqft']);
               })
               var cur_sqft = $('#p-job-sqft').val();
               sum_sqft += parseInt(cur_sqft);
-				console.log(sum_sqft);
+				console.log('sum_sqft=' + sum_sqft);
               if (sum_sqft > currently_sqft){
                 flag = true;
               }else{  
