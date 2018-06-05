9b748ddfcb3de1436f25b1ffebdb67d084ebef83
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-06-05 16:05:38 -0400

    a

--- admin/js/project_add.js
+++ admin/js/project_add.js
@@ -277,19 +277,26 @@ $(document).ready(function() {
 		$('input').removeClass('is-invalid');
 		$('#loadOver').fadeIn(500);
 
-		if ($('input[name=job_name]') == '') {
+		alert("clicked 1");
+		if ($('input[name=uid]').val() < 1) {
+			alert("No project owner selected.");
+			return;
+		}
+		alert("clicked 2");
+		if ($('input[name=job_name]').val() == '') {
 			$('input[name=job_name]').addClass('is-invalid');
 			$('input[name=job_name]').focus();
 			alert("You must have a Job Name.");
 			return;
 		}
-		alert("clicked 2");
-		if ($('input[name=acct_rep]') < 1) {
+		alert("clicked 3");
+		if ($('input[name=acct_rep]').val() < 1) {
 			$('input[name=acct_rep]').addClass('is-invalid');
 			$('input[name=acct_rep]').focus();
 			alert("You must specify an Account Rep.");
 			return;
 		}
+		alert("clicked 4");
 		if ($('input[name=install_date]').val() == '') {
 			$('input[name=install_date]').val('2200-01-01');
 		} else {
