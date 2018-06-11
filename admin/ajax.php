5e3c64f68daeb1acf001e46ab768f7c0b4c408a2
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-06-11 08:22:20 -0400

    a

--- admin/ajax.php
+++ admin/ajax.php
@@ -1706,10 +1706,11 @@ if ($action=="saw_list") {
 		?>
 		<hr>
 		<div class="row">
-			<div class="col-9 col-md-6 text-primary text-uppercase"><?= $results['job_name']; ?></div>
-			<div class="col-9 col-md-2 text-primary"><?= $results['quote_num']; ?></div>
-			<div class="col-9 col-md-2 text-primary"><?= $results['order_num']; ?></div>
-			<div  class="col-2">
+			<div class="col-md-5 text-primary"><?= $results['install_date']; ?></div>
+			<div class="col-md-5 text-primary text-uppercase"><?= $results['job_name']; ?></div>
+			<div class="col-md-2 text-primary"><?= $results['quote_num']; ?></div>
+			<div class="col-md-2 text-primary"><?= $results['order_num']; ?></div>
+			<div  class="col-md-1">
 				<div id="<?= $results['id']; ?>" class="btn btn-primary w-100" onClick="viewThisProject(this.id,<?= $results['uid']; ?>);">
 					<i class="fas fa-eye"></i>
 				</div>
