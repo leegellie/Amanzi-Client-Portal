1751150dfdb056fc247eef468db88137f8d83a8a
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-06-12 17:28:06 -0400

    q

--- admin/menu.php
+++ admin/menu.php
@@ -11,53 +11,44 @@ if ($access_level < 11) {
 		<a class="navbar-brand" href="#"><img src="../images/icon.png" style="max-width:52px"></a>
 		<div class="collapse navbar-collapse" id="navbarCollapse">
 			<ul class="navbar-nav m-auto">
+<?
+	if ($access_level < 4) {
+?>
 				<li class="nav-item py-3 px-4">
 					<a class="element" href="dashboard.php"><span class="icon-home"></span></a>
 				</li>
 <?
-	if ($_SESSION['id'] == 1 || $_SESSION['id'] == 14) {
+		if ($_SESSION['id'] == 1 || $_SESSION['id'] == 14 || $_SESSION['id'] == 1447) {
 ?>
 				<li class="nav-item py-3 px-4 dropdown">
 					<a class="dropdown-toggle text-white" id="userdropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin</a>
 					<div class="dropdown-menu" aria-labelledby="userdropdown">
+<?
+			if ($_SESSION['id'] != 1447) {
+?>
 						<a class="dropdown-item text-dark" href="admin.php?profit">Profit/Loss</a>
+<?
+			}
+?>
 						<a class="dropdown-item text-dark" href="admin.php?stats">Stats</a>
 					</div>
 				</li>
 <?
-	}
+		}
 ?>
 				<li class="nav-item py-3 px-4 dropdown">
 					<a class="dropdown-toggle text-white" id="userdropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Customers</a>
 					<div class="dropdown-menu" aria-labelledby="userdropdown">
 						<a class="dropdown-item text-dark" href="users.php?edit">View Customers</a>
-<?
-	if ($access_level < 4) {
-?>
 						<a class="dropdown-item text-dark" href="users.php?add">Add Customers</a>
-<?
-	}
-	if ($access_level < 11) {
-?>
 					</div>
 				</li>
 				<li class="nav-item py-3 px-4 dropdown">
 					<a class="dropdown-toggle text-white" id="pjtdropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projects</a>
 					<div class="dropdown-menu" aria-labelledby="pjtdropdown">
 						<a class="dropdown-item text-dark" href="projects.php?edit">View Projects</a>
-<?
-	}
-	if ($access_level < 4) {
-?>
 						<a class="dropdown-item text-dark" href="projects.php?add">Add a Project</a>
-<?
-	}
-	if ($access_level == 1 || $access_level == 3) {
-?>
 						<a class="dropdown-item text-dark" href="projects.php?entry">To Enter</a>
-<?
-	}
-?>
 						<a class="dropdown-item text-dark" href="projects.php?templates">Templates</a>
 						<a class="dropdown-item text-dark" href="projects.php?programming">Programming</a>
 						<a class="dropdown-item text-dark" href="projects.php?saw">Saw</a>
@@ -65,7 +56,6 @@ if ($access_level < 11) {
 						<a class="dropdown-item text-dark" href="projects.php?polishing">Polishing</a>
 						<a class="dropdown-item text-dark" href="projects.php?installs">Installs</a>
 						<a class="dropdown-item text-dark" href="projects.php?timeline">Timeline</a>
-						<!--<a class="dropdown-item text-dark" onclick="$('#qr_scanner').modal('show')">QR Test</a>-->
 					</div>
 				</li>
 				<li class="nav-item py-3 px-4 dropdown">
@@ -73,12 +63,11 @@ if ($access_level < 11) {
 					<div class="dropdown-menu" aria-labelledby="toolsdropdown">
 						<a class="dropdown-item text-dark" href="/admin/projects.php?marbgran">Marble/Granite</a>
 						<a class="dropdown-item text-dark" href="/admin/projects.php?quartz">Quartz</a>
-
-						<!--<a class="dropdown-item text-dark" onClick="window.open('/login.php')" href="#">Materials Pricing</a>-->
 					</div>
 				</li>
 
 <?
+	}
 	if ($access_level == 6 || $access_level == 1) {
 ?>
 				<li class="nav-item py-3 px-4 dropdown">
@@ -87,46 +76,19 @@ if ($access_level < 11) {
 						<a class="dropdown-item text-dark" href="/admin/materials.php">Material Needed</a>
 						<a class="dropdown-item text-dark" href="/admin/admin.php?marble">Marble/Granite</a>
 						<a class="dropdown-item text-dark" href="/admin/admin.php?quartz">Quartz</a>
-
-						<!--<a class="dropdown-item text-dark" onClick="window.open('/login.php')" href="#">Materials Pricing</a>-->
 					</div>
 				</li>
-
 <?
 	}
-	if ($access_level < 11) {
 ?>
 				<li class="nav-item py-3 px-4 dropdown">
 					<a class="dropdown-toggle text-white" id="toolsdropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Staff</a>
 					<div class="dropdown-menu" aria-labelledby="toolsdropdown">
 						<a class="dropdown-item text-dark" href="/admin/time_off.php">Time Off</a>
-
-						<!--<a class="dropdown-item text-dark" onClick="window.open('/login.php')" href="#">Materials Pricing</a>-->
 					</div>
-				</li>
-				
-<?
-	}
-	if ($access_level == 1) {
-?>
-<!-- 				<li class="nav-item py-3 px-4 dropdown">
-						<a class="" href="/admin/testing.php">Testing</a>
-				</li> -->
-
-				<!--<li class="nav-item py-3 px-4 dropdown">
-					<a class="dropdown-toggle text-white" id="toolsdropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tools</a>
-					<div class="dropdown-menu" aria-labelledby="toolsdropdown">
-						<a class="dropdown-item text-dark" target="_blank" href="/waiver/">Visitor's Waiver</a>
-						<a class="dropdown-item text-dark" target="_blank" href="http://calc.amanzigranite.com/">Materials Calculator</a>
-						<a class="dropdown-item text-dark" target="_blank" href="http://price.amanzigranite.com/">Materials Pricing</a>
-
-					</div>
-				</li>-->
-<?
-	}
-?>
+				</li>			
 			</ul>
-			<h3 class="text-primary">Hi <?= $first_name ?></h3>
+			<h3 class="text-primary">Hi <?= $first_name ?> </h3>
 			<a href="/logout.php" class="btn nav-item btn-secondary my-auto text-white">Log Out</a>
 		</div>
 	</nav>
