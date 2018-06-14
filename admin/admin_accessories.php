c35aa217f5076ded4a2b0dd4d2040e30a42f991d
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-06-13 20:29:02 -0400

    a

--- admin/admin_accessories.php
+++ admin/admin_accessories.php
@@ -60,7 +60,7 @@ function escapeHtml (string) {
     return entityMap[s];
   });
 }
-									$name_string = escapeHtml (<?= $results['accs_name'] ?>);
+									$name_string = escapeHtml(<?= $results['accs_name'] ?>);
 								</script>
 		<?
 		$editString = $results['accs_id'] . ",'" . $results['accs_code'] . "'," . $results['accs_model'] . "'," . $results['accs_cost'] . ',' . $results['accs_price'] . ',' . $results['accs_status'] . ',' . $results['accs_width'] . ',' . $results['accs_depth'];
