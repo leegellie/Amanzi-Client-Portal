1e3b8f184609c120287bf2deff23a60de12a862d
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-06-20 17:50:11 -0400

    a

--- admin/ajax.php
+++ admin/ajax.php
@@ -898,10 +898,11 @@ if ($action=="get_materials_needed") {
 	function show_pull_head($result) {
 		$head_arr = '
 			<div class="w-100 d-flex">
-				<div class="col-md-2"><h3>' . format_date($result['install_date']) . '</h3></div>
+				<div class="col-md-1"><h3>' . format_date($result['install_date']) . '</h3></div>
 				<div class="col-9 col-md-8 text-primary"><h3>' . $result['order_num'] . ' - ' . $result['job_name'] . '</h3></div>
-				<div class="col-2 col-md-2 text-right">
+				<div class="col-2 col-md-3 text-right">
 					<div id="' . $result['pid'] . '" class="btn btn-sm btn-primary" onClick="$(\'#instDetails\').html(\'\');viewThisProject(this.id,'. $result['uid'] .');"><span class="hidden-md-down">View </span><i class="fas fa-eye"></i></div>
+					<div id="' . $result['id'] . '" class="btn btn-sm btn-success" onClick="material_delivered(this.id);"><span class="hidden-md-down">Delivered </span><i class="fas fa-truck"></i></div>
 				</div>
 			</div>
 			<hr>';
@@ -920,15 +921,7 @@ if ($action=="get_materials_needed") {
 					<div class="col-2 text-danger"><b>MATERIALS ON HOLD</b></div>
 					<div class="col-2 btn btn-sm btn-danger mr-2" onClick="mat_release_modal(' . $_SESSION['id'] .',' . $result['id'] . ',' . $result['pid'] . ')">Release Hold <i class="fas fa-ban"></i></div>
 				</div>'; 
-			} else {
-				$tmp_arr .= '
-					<div class="col-4 text-right">
-						<div id="' . $result['id'] . '" class="btn btn-sm btn-primary" onClick="$(\'#instDetails\').html(\'\');viewThisProject(this.id,' . $result['uid'] . ');"><span class="hidden-md-down">View </span><i class="fas fa-eye"></i></div>
-						<div id="' . $result['id'] . '" class="btn btn-sm btn-success" onClick="material_delivered(this.id);"><span class="hidden-md-down">Delivered </span><i class="fas fa-truck"></i></div>
-					</div>
-				</div>
-			</div>
-			<hr>'; 
+			}
 		}
 		return $tmp_arr;
 	}
