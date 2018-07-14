0e664ad4a708c07fa3333baf2b3c24abfb059edf
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-07-14 10:53:21 -0400

    a

--- admin/ajax.php
+++ admin/ajax.php
@@ -3903,26 +3903,24 @@ if ($action=="timelines_list") {
 		}
 	}
 	echo 	'		</div>';
+	echo 	'		<div class="col-12 col-md-3"><h3>Install Progress</h3>';
+	foreach($installs_pro as $t) {
+		$stat = $t['job_status'];
+		if ($stat > 80 && $stat != 84 && $stat != 85) {
+			$status = instStatus($stat);
+			production_button($t,$status);
+		}
+	}
+	echo 	'		</div>';
 	echo 	'		<div class="col-12 col-md-3"><h3>Install Complete/Incomplete</h3>';
 	foreach($installs_pro as $t) {
 		$stat = $t['job_status'];
-		if ($stat ==84 || $stat == 85) {
+		if ((time()-(60*60*24*4)) > strtotime($t['install_date']) && $stat ==84 || $stat == 85) {
 			$status = instStatus($stat);
 			production_button($t,$status);
 		}
 	}
 	echo 	'		</div>';
-	echo 	'		<div class="col-12 col-md-3"><h3>Install On Hold</h3>';
-	foreach($installs_pro as $t) {
-		$stat = $t['job_status'];
-		if ($stat == 89) {
-			$status = instStatus($stat);
-			production_button($t,$status);
-		}
-	}
-	echo 	'		</div>';
-
-
 
 	echo 	'	</div>';
 	echo 	'	</div>';
