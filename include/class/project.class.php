49dea6d6567b8fe2da800b3414c6e932fecb4838
LEE GELLIE <LEEGELLIE@GMAIL.COM>
2018-07-14 11:51:13 -0400

    a

--- include/class/project.class.php
+++ include/class/project.class.php
@@ -1929,7 +1929,7 @@ class project_action {
 				FROM projects 
 				JOIN status 
 				  ON status.id = projects.job_status 
-			   WHERE projects.install_date < '2200-01-01' 
+			   WHERE projects.install_date < '2100-01-01' 
 			     AND projects.job_status > 24
 				 AND projects.job_status < 84
 				 AND projects.isActive = 1
@@ -1984,6 +1984,7 @@ class project_action {
 			return $this->_message;
 		}  
 	}
+
 
 	// SELECT PROJECT DATA BASED ON LIST SELECT 
 	public function project_data_fetch($a) {
